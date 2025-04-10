2cea29d494150641526102eceed8f1394e50e34f
Naufal Hanif <naufal.hanif2525@gmail.com>
2025-04-10 11:35:07 +0700

    Update pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,59 +1,9 @@
 "use client";  // Marks this component as a Client Component
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Signin() {
     const router = useRouter();
-
-    // Typing effect configuration
-    const texts = ["#ManageYourDailyTasksEasily", "#MakeYourLifeBetter"];
-    const [displayedText, setDisplayedText] = useState("");
-    const [textIndex, setTextIndex] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [pause, setPause] = useState(false);
-
-    // Typing and deleting logic
-    useEffect(() => {
-        if (pause) return;
-
-        const currentText = texts[textIndex];
-        const speed = isDeleting ? 60 : 80;
-
-        const timeout = setTimeout(() => {
-            if (!isDeleting) {
-                const next = currentText.slice(0, displayedText.length + 1);
-
-                setDisplayedText(next);
-
-                if (next === currentText) {
-                    setPause(true);
-                    setTimeout(() => setIsDeleting(true), 1000);
-                }
-            } 
-            else {
-                const prev = currentText.slice(0, displayedText.length - 1);
-
-                setDisplayedText(prev);
-
-                if (prev === "") {
-                    setIsDeleting(false);
-                    setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
-                }
-            }
-        }, speed);
-
-        return () => clearTimeout(timeout);
-    }, [displayedText, isDeleting, pause, textIndex]);
-
-    // Delay handler between phrases
-    useEffect(() => {
-        if (pause) {
-            const pauseTimeout = setTimeout(() => setPause(false), 3000);
-
-            return () => clearTimeout(pauseTimeout);
-        }
-    }, [pause]);
 
     return (
         <div className="flex flex-col w-full h-full items-center">
@@ -87,23 +37,18 @@ export default function Home() {
                 </button>
             </div>
 
-            {/* Welcome section */}
-            <div className="front absolute w-full h-[60%] top-[0%] flex flex-col items-center justify-center">
-                <h1 className="text-[64pt] m-0 text-center font-bold">Welcome</h1>
-                <h2 className="text-[16pt] m-0 text-center flex items-center gap-[0.4em]">
-                    TaskStack.
-                    <ins className="hastag">{displayedText}</ins>
-                </h2>
-            </div>
-
-            {/* Button */}
-            <div className="front absolute w-full h-[30%] top-[60%] gap-[1.2em] flex flex-row items-center justify-center">
-                {/* TODO: Add a function to this button to handle event  */}
-                <button className="solid-button text-[12pt] w-[200px]">Let's Get Started</button>
+            {/* Main container */}
+            <div className="w-[100vw] h-[100vh] top-[0%] fixed flex items-center justify-center">
+                <div className="glass form w-[80vw] flex flex-col gap-[1.2em] items-center justify-center">
+                    <h1 className="text-[16pt] font-semibold">About</h1>
+                    <p className="text-[12pt] w-full text-center">
+                        TaskStack is a modern and intuitive website designed to help users efficiently manage their daily to-do lists. With a clean and responsive interface, TaskStack makes it easy to add, edit, and complete tasks while offering a seamless user experience across all devices. Enhanced with interactive animations and smart reminders, TaskStack is more than just a productivity tool—it&apos;s your daily companion for staying focused and achieving your goals.
+                    </p>
+                </div>
             </div>
 
             {/* Footer */}
-            <div className="front footer absolute w-full h-[10%] top-[100%] flex flex-col items-center justify-end">
+            <div className="front footer fixed w-full h-[10%] top-[85%] flex flex-col items-center justify-end">
                 <p className="text-[8pt]">Copyright &copy; 2025 TaskStack. All rights reserved.</p>
             </div>
 
@@ -111,7 +56,7 @@ export default function Home() {
             <div className="background absolute flex justify-center w-[120vw] h-[65vh] overflow-hidden">
                 <div className="circle-silhouette-outer translate-y-[2em] overflow-hidden absolute w-full h-[120%] rounded-t-[100%]"></div>
                 <div className="circle-silhouette-outer translate-y-[2em] overflow-hidden absolute w-full h-[120%] flex justify-center items-center rounded-t-[100%] blur-[12px] brightness-[120%]">
-                    <div className="circle-silhouette-inner translate-y-[-2px] w-[120%] h-[102%] rounded-t-[100%] blur-[32px]"></div>
+                    <div className="circle-silhouette-inner translate-y-[-2px] rounded-t-[100%] blur-[32px]"></div>
                 </div>
             </div>
         </div>
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,48 @@
+"use client";  // Marks this component as a Client Component
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Signup() {
+    const router = useRouter();
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="flex flex-col w-full h-full items-center">
+            {/* Header */}
+            <div className="header-text fixed w-full flex flex-row top-[4%] items-center justify-between">
+                <button className="link-button text-[12pt] font-semibold" onClick={() => router.back()}>&lt;-</button>
+            </div>
+
+            {/* Form */}
+            <div className="w-[100vw] h-[100vh] top-[0%] fixed flex items-center justify-center">
+                <div className="glass form w-[420px] fixed flex flex-col gap-[3.2em] items-center justify-center">
+                    <h1 className="text-[16pt] font-semibold">Sign Up</h1>
+                    <div className="flex flex-col items-center justify-left gap-[1.2em]">
+                        <input type="text" id="username" className="input w-full text-[12pt]" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+                        <input type="email" id="email" className="input w-full text-[12pt]" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                        <input type="password" id="password" className="input w-full text-[12pt]" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                    </div>
+                    {/* TODO: Add a function to this button to handle event  */}
+                    <button className="solid-button text-[12pt] w-[160px]">Sign Up</button>
+                </div>
+            </div>
+
+            {/* Footer */}
+            <div className="front footer fixed w-full h-[10%] top-[85%] flex flex-col items-center justify-end">
+                <p className="text-[8pt]">Copyright &copy; 2025 TaskStack. All rights reserved.</p>
+            </div>
+
+            {/* Purple cicle silhouette background */}
+            <div className="background absolute flex justify-center w-[120vw] h-[65vh] overflow-hidden">
+                <div className="circle-silhouette-outer translate-y-[2em] overflow-hidden absolute w-full h-[120%] rounded-t-[100%]"></div>
+                <div className="circle-silhouette-outer translate-y-[2em] overflow-hidden absolute w-full h-[120%] flex justify-center items-center rounded-t-[100%] blur-[12px] brightness-[120%]">
+                    <div className="circle-silhouette-inner translate-y-[-2px] w-[120%] h-[102%] rounded-t-[100%] blur-[32px]"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
